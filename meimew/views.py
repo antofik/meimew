13d160febc115c018dac2a39b29f3775a446c6c9
antofik <antofik@gmail.com>
2014-09-19 19:45:29 +0400

    Update views.py

--- meimew/views.py
+++ meimew/views.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.http import HttpResponse, Http404, HttpResponseBadRequest
 from django.template import RequestContext
 from django.shortcuts import render_to_response, redirect
