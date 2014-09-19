f894791a5547fb9f97ba29d5c4db56c29db8c8a2
antofik <antofik@gmail.com>
2014-09-19 11:04:56 +0400

    [/] Manage scripts

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+mkdir logs
+sudo ln -s /server/www/meimew/nginx.conf /etc/nginx/conf.d/meimew.conf
+sudo ln -s /server/www/meimew/supervisor.conf /etc/supervisor/conf.d/meimew.conf
